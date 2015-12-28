61e48cc77e2ebfe172aaf22ccb574112df057283
jiexu10 <jie.xu.10@gmail.com>
2015-12-27 20:03:24 -0500

    days controller new and create added

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -3,4 +3,26 @@ class DaysController < ApplicationController
     @day = Day.find(params[:id])
     @season = @day.season
   end
+
+  def new
+    @day = Day.new
+  end
+
+  def create
+    @season = Season.find(params[:season_id])
+    @day = Day.new(day_params)
+    @day.season = @season
+
+    if @day.save
+      redirect_to day_path(@day), notice: 'Day added successfully'
+    else
+      flash.now[:error] = "#{@day.errors.full_messages}"
+      render :new
+    end
+  end
+
+  private
+  def day_params
+    params.require(:day).permit(:end_year)
+  end
 end
